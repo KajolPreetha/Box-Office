1420bb38d0a0992d6bcefc5878be6235a47bab32
kajol <kajolpreethababu@gmail.com>
2021-01-05 13:30:36 +0530

    added navbar and created page components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Starred from "./pages/Starred";
+
 
 function App() {
   return (
-    <div >
-      BOX OFFICE
+    <div>
+      <Nav />
+      <Switch>
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+
+        <Route>
+          <h1>ERROR 404</h1>
+        </Route>
+      </Switch>
     </div>
+    
   );
 }
 
