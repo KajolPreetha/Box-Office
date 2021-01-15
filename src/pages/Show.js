420518c2904f861aafa56308fd0f27657c32137b
kajol <kajolpreethababu@gmail.com>
2021-01-15 14:19:20 +0530

    created show page components and populated them with data

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useReducer } from 'react'
 import {useParams} from "react-router-dom"
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
 
 const reducer = (prevState, action) => {
@@ -55,8 +59,6 @@ const Show = () => {
         }
     }, [id])
 
-     console.log("show", show);
-
      if (isLoading) {
          return <div>Data is being loaded</div>
      }
@@ -66,7 +68,29 @@ const Show = () => {
     
     return (
         <div>
-            THis is show page
+            <ShowMainData
+                image={show.image}
+                name={show.name}
+                rating={show.rating}
+                summary={show.summary}
+                tags={show.genres}
+            />
+            <div>
+                <h2>Details</h2>
+                <Details
+                    status={show.status}
+                    network={show.network}
+                    premiered={show.premiered}
+                />
+            </div>
+            <div>
+                <h2>Seasons</h2>
+                <Seasons seasons={show._embedded.seasons} />
+            </div>
+            <div>
+                <h2>Cast</h2>
+                <Cast cast={show._embedded.cast} />
+            </div>
         </div>
     )
 }
